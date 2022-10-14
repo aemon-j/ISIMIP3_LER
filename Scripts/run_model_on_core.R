de52294b3d37d7e172578dfab9e62e42b3b00de1
Jorrit Mesman <jpmesman@gmail.com>
2022-10-14 15:42:28 +0200

    Use of parallel package instead of jobs for parallel runs

--- Scripts/run_model_on_core.R
+++ Scripts/run_model_on_core.R
@@ -1,37 +1,29 @@
 # Script called from 4_Run_all_simulations.R
 # simulates a certain selection of sim_tasks
 
-# Imported packages are not transferred to the job, so need to be imported again. 
-library(data.table)
-library(LakeEnsemblR)
-
-if(!exists("core_job")){
-  stop("core_job should exist!")
-}
-
-setDT(sim_tasks)
-
-sim_tasks = sim_tasks[Core == core_job]
-
-add_to_report(file.path(folder_root, folder_report), report_name, 4L, core_job,
-              paste0("Tasks (", nrow(sim_tasks), "): ", paste(sim_tasks[, Lakes],
-                                                              sim_tasks[, GCM],
-                                                              sim_tasks[, Scen],
-                                                              sep = "_",
-                                                              collapse = ", ")))
-
-for(i in seq_len(nrow(sim_tasks))){
-  sim_folder = file.path(folder_root,
-                         folder_data,
-                         sim_tasks[i, Lakes],
-                         tolower(sim_tasks[i, GCM]),
-                         as.character(sim_tasks[i, Scen]))
-  
-  setwd(sim_folder)
-  
-  run_ensemble(config_file = "LakeEnsemblR.yaml",
-               model = models_to_run)
+run_model_on_core = function(sim_tasks, core_job){
+  sim_tasks = sim_tasks[Core == core_job]
   
   add_to_report(file.path(folder_root, folder_report), report_name, 4L, core_job,
-                paste0("Completed: ", paste(sim_tasks[i, Lakes], sim_tasks[i, GCM], sim_tasks[i, Scen], sep = "_")))
+                paste0("Tasks (", nrow(sim_tasks), "): ", paste(sim_tasks[, Lakes],
+                                                                sim_tasks[, GCM],
+                                                                sim_tasks[, Scen],
+                                                                sep = "_",
+                                                                collapse = ", ")))
+  
+  for(i in seq_len(nrow(sim_tasks))){
+    sim_folder = file.path(folder_root,
+                           folder_data,
+                           sim_tasks[i, Lakes],
+                           tolower(sim_tasks[i, GCM]),
+                           as.character(sim_tasks[i, Scen]))
+    
+    setwd(sim_folder)
+    
+    run_ensemble(config_file = "LakeEnsemblR.yaml",
+                 model = models_to_run)
+    
+    add_to_report(file.path(folder_root, folder_report), report_name, 4L, core_job,
+                  paste0("Completed: ", paste(sim_tasks[i, Lakes], sim_tasks[i, GCM], sim_tasks[i, Scen], sep = "_")))
+  }
 }
