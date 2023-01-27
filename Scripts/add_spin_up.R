5c2e6ae15546f10ba733e59fd47ddd6f2d650c73
Jorrit Mesman <jpmesman@gmail.com>
2023-01-27 18:27:36 +0100

    Improved add_spin_up script

--- Scripts/add_spin_up.R
+++ Scripts/add_spin_up.R
@@ -11,17 +11,35 @@ add_spin_up = function(folder, spin_up_years = 0L){
     stop("add_spin_up function can only run with an integer number of years. E.g. '3L'")
   }
   
+  the_start_date = get_yaml_multiple(file.path(folder, "LakeEnsemblR.yaml"),
+                                     key1 = "time", key2 = "start")
+  the_start_date = as.POSIXct(the_start_date, format = "%Y-%m-%d%H:%M:%S")
+  
   ### Meteo file
   df_meteo = fread(file.path(folder, "meteo.csv"))
   
-  # Get row numbers that are first three years
-  ind = max(df_meteo[, which(datetime < (df_meteo[1L, datetime] + years(spin_up_years)))])
+  met_time_step = LakeEnsemblR:::get_meteo_time_step(file.path(folder, "meteo.csv")) # in sec
+  
+  # Cut to start on the_start_date and repeat the first spin_up_years years
+  df_meteo = df_meteo[datetime >= the_start_date]
+  ind = df_meteo[datetime < (the_start_date + years(spin_up_years)), .N]
   
   # Place in front
   df_meteo = df_meteo[c(1:ind, 1:.N)]
   
-  # Subtract three years
+  # Subtract the years
   df_meteo[1:ind, datetime := datetime - years(spin_up_years)]
+  
+  # Deal with leap years: remove generated NAs and fill gaps with date before
+  df_meteo = df_meteo[!is.na(datetime)]
+  df_meteo = merge.data.table(df_meteo,
+                              list("datetime" = seq.POSIXt(from = df_meteo[1L, datetime],
+                                              to = df_meteo[.N, datetime],
+                                              by = met_time_step)),
+                              by = "datetime",
+                              all.y = TRUE)
+  setnafill(df_meteo, type = "locf")
+  setnafill(df_meteo, type = "nocb") # Precaution if starting date is a leap date
   
   df_meteo[, datetime := format(datetime, "%Y-%m-%d %H:%M:%S")]
   fwrite(df_meteo, file.path(folder, "meteo.csv"))
@@ -32,6 +50,5 @@ add_spin_up = function(folder, spin_up_years = 0L){
                       value = df_meteo[1L, datetime],
                       verbose = F)
   
-  
-  # (if inflow will be part of ISIMIP3, then this will be needed as well)
+  # (if inflow will be part of ISIMIP3, then this will need updating as well)
 }
