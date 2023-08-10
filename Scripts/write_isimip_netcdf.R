2b167605fe844533c6261a7830a2001304c185d6
Jorrit Mesman <jpmesman@gmail.com>
2023-08-10 13:56:57 +0200

    Improvement writing ISIMIP output nc and fix FLake NAs

--- Scripts/write_isimip_netcdf.R
+++ Scripts/write_isimip_netcdf.R
@@ -5,24 +5,27 @@
 # https://protocol.isimip.org/#reporting-model-results
 
 write_isimip_netcdf = function(vals, time, deps = NULL, var_name, var_unit,
-                                  file_name, lat, lon,
-                                  ref_year = "1601", compression = 5L){
+                               var_longname,
+                               file_name, lat, lon,
+                               ref_year = "1601", compression = 5L){
   
   ref_time = as.POSIXct(paste0(ref_year, "-01-01 00:00:00"), tz = "UTC")
   ndays = as.numeric(difftime(time, ref_time, units = "days"))
   
   # Define lon and lat dimensions
-  lon1 = ncdim_def("lon", "degrees_east", vals = as.double(ifelse(longitude >= 0, longitude, longitude + 360)))
-  lat2 = ncdim_def("lat", "degrees_north", vals = as.double(lat))
+  lon1 = ncdim_def("lon", "degrees_east", vals = as.double(ifelse(longitude >= 0, longitude, longitude + 360)),
+                   longname = "Longitude")
+  lat2 = ncdim_def("lat", "degrees_north", vals = as.double(lat), longname = "Latitude")
   
   # Time dimension
-  timedim = ncdf4::ncdim_def("time", units = paste0("days since ", ref_year, "-01-01 00:00:00"),
-                             vals = as.double(ndays), calendar = "proleptic_gregorian")
+  timedim = ncdim_def("time", units = paste0("days since ", ref_year, "-01-01 00:00:00"),
+                      vals = as.double(ndays), calendar = "proleptic_gregorian",
+                      unlim = T)
   
-  # Depth dimension (positive numbers below surface)
+  # Levlak dimension (positive numbers below surface)
   if(!is.null(deps)){
-    depthdim = ncdim_def("levlak", units = "meters", vals = as.double((-deps)),
-                         longname = "Depth from surface")
+    levlakdim = ncdim_def("levlak", units = "-", vals = as.numeric(seq_along(deps)),
+                          longname = "Vertical Water Layer Index")
   }
   
   # What to do with NA data?
@@ -33,13 +36,18 @@ write_isimip_netcdf = function(vals, time, deps = NULL, var_name, var_unit,
   if(is.null(ncol(vals)) & is.null(deps)){
     nc_var = ncvar_def(var_name, var_unit,
                        list(lon1, lat2, timedim),
-                       fillvalue, var_name,
+                       fillvalue, var_longname,
                        prec = "float", compression = compression, shuffle = FALSE)
   }else{
     nc_var = ncvar_def(var_name, var_unit,
-                       list(lon1, lat2, depthdim, timedim),
-                       fillvalue, var_name,
+                       list(lon1, lat2, levlakdim, timedim),
+                       fillvalue, var_longname,
                        prec = "float", compression = compression, shuffle = FALSE)
+    depth_var = ncvar_def("depth", "m",
+                          list(levlakdim),
+                          missval = NULL,
+                          longname = "Depth of Vertical Layer Center Below Surface",
+                          prec = "float", compression = compression, shuffle = FALSE)
   }
   
   # If file exists - delete it
@@ -48,24 +56,53 @@ write_isimip_netcdf = function(vals, time, deps = NULL, var_name, var_unit,
   }
   
   # Create and input data into the netCDF file
-  ncout = nc_create(file_name, list(var_name = nc_var), force_v4 = T)
+  if(is.null(ncol(vals)) & is.null(deps)){
+    ncout = nc_create(file_name, list(var_name = nc_var), force_v4 = T)
+  }else{
+    ncout = nc_create(file_name, list(depth = depth_var,
+                                      var_name = nc_var), force_v4 = T)
+  }
+  
+  # Add Global attributes
+  ncatt_put(ncout, varid = 0, attname = "contact", attval = "Jorrit Mesman <jorrit.mesman@ebc.uu.se>")
+  ncatt_put(ncout, varid = 0, attname = "institution", attval = "Uppsala University (UU)")
+  ncatt_put(ncout, varid = 0, attname = "comment", attval = "Data prepared for ISIMIP3b")
+  
+  # Add standard name to the variable
+  ncatt_put(ncout, varid = var_name, attname = "standard_name", attval = var_name)
+  
+  # Add standard names and axis names to the dimensions
+  ncatt_put(ncout, varid = "lon", attname = "standard_name", attval = "longitude")
+  ncatt_put(ncout, varid = "lon", attname = "axis", attval = "X")
+  ncatt_put(ncout, varid = "lat", attname = "standard_name", attval = "latitude")
+  ncatt_put(ncout, varid = "lat", attname = "axis", attval = "Y")
+  ncatt_put(ncout, varid = "time", attname = "standard_name", attval = "time")
+  ncatt_put(ncout, varid = "time", attname = "axis", attval = "T")
+  
+  if(!is.null(deps)){
+    ncatt_put(ncout, varid = "levlak", attname = "standard_name", attval = "water_layer")
+    ncatt_put(ncout, varid = "levlak", attname = "positive", attval = "down")
+    ncatt_put(ncout, varid = "levlak", attname = "axis", attval = "Z")
+  }
   
   # Add tryCatch ensure that it closes netCDF file
   result = tryCatch({
     if(is.null(ncol(vals)) & is.null(deps)){
       # Add 1D variable
       ncvar_put(ncout, nc_var, vals)
-      ncatt_put(ncout, nc_var, attname = "coordinates",
-                attval = c("lon lat"))
+      # ncatt_put(ncout, nc_var, attname = "coordinates",
+      #           attval = c("lon lat"))
       ncvar_change_missval(ncout, nc_var, missval = fillvalue)
     }else{
       # Add 2D variable
+      ncvar_put(nc = ncout, varid = depth_var, vals = as.double(-deps))
       ncvar_put(nc = ncout, varid = nc_var, vals = vals)
-      ncatt_put(ncout, "levlak", attname = "coordinates", attval = c("levlak"))
-      ncatt_put(ncout, nc_var, attname = "coordinates",
-                attval = c("lon lat levlak"))
-      ncvar_change_missval(ncout, nc_var, missval = fillvalue)
       
+      ncatt_put(ncout, "depth", attname = "standard_name", attval = "depth")
+      ncatt_put(ncout, "depth", attname = "positive", attval = "down")
+      ncatt_put(ncout, "depth", attname = "axis", attval = "Z")
+      
+      ncvar_change_missval(ncout, nc_var, missval = fillvalue)
     }
   }, warning = function(w){
     return_val = "Warning"
